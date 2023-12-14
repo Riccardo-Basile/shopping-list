241dfbca67222b310e8cacdc2fc94c4a7f53aa3d
Riccardo-Basile <riccardo.guitarman@gmail.com>
2023-12-14 23:17:20 +0100

    Remove items from local storage

--- script.js
+++ script.js
@@ -75,21 +75,37 @@ function getItemsFromStorage() {
 
   return itemsFromStorage;
 }
-
-function removeItem(e) {
+function onClickItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
-    if (confirm('Are you sure?')) {
-      e.target.parentElement.parentElement.remove();
-
-      checkUI();
-    }
+    removeItem(e.target.parentElement.parentElement);
   }
 }
+function removeItem(item) {
+  if (confirm('Are you sure?')) {
+    // Remove item from DOM
+    item.remove();
+    // Remove item from storage
+    removeItemFromStorage(item.textContent);
 
-function clearItems(e) {
+    checkUI();
+  }
+}
+function removeItemFromStorage(item) {
+  let itemsFromStorage = getItemsFromStorage();
+  // Filter out item to be removed
+  itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
+
+  // Re-set to local storage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+function clearItems() {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+
+  // Clear from local storage
+  localStorage.removeItem('items');
 
   checkUI();
 }
@@ -124,7 +140,7 @@ function checkUI() {
 function init() {
   // EVENT LISTENERS
   itemForm.addEventListener('submit', onAddItemSubmit);
-  itemList.addEventListener('click', removeItem);
+  itemList.addEventListener('click', onClickItem);
   clearBtn.addEventListener('click', clearItems);
   itemFilter.addEventListener('input', filterItems);
   document.addEventListener('DOMContentLoaded', displayItems);
