1329a57943686c39c26bf87d90290d15084e4c00
Riccardo-Basile <riccardo.guitarman@gmail.com>
2023-12-16 18:34:32 +0100

    Prevent duplicate items

--- script.js
+++ script.js
@@ -30,7 +30,13 @@ function onAddItemSubmit(e) {
     itemToEdit.classList.remove('edit-mode');
     itemToEdit.remove();
     isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert('That item already exist');
+      return;
+    }
   }
+
   // Create item DOM elements
   addItemToDOM(newItem);
 
@@ -93,6 +99,11 @@ function onClickItem(e) {
   } else {
     setItemToEdit(e.target);
   }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+  return itemsFromStorage.includes(item);
 }
 
 function setItemToEdit(item) {
